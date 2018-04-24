1966f51720580418e52a4d987dfed16ff1d14bd0
Tom Gross <itconsense@gmail.com>
2018-04-24 07:15:36 +0000

    Fix unlinking relation (after fixing https://github.com/plone/Products.CMFPlone/issues/2384)

--- plone/app/iterate/dexterity/policy.py
+++ plone/app/iterate/dexterity/policy.py
@@ -10,10 +10,12 @@ from plone.app.iterate.event import CheckinEvent
 from plone.app.iterate.interfaces import CheckinException
 from plone.app.iterate.interfaces import IObjectCopier
 from plone.app.iterate.util import get_storage
+from zc.relation.interfaces import ICatalog
 from zope.component import adapter
 from zope.component import queryAdapter
 from zope.component import queryUtility
 from zope.event import notify
+from zope.intid.interfaces import IIntIds
 
 
 @adapter(IDexterityIterateAware)
@@ -34,28 +36,19 @@ class CheckinCheckoutPolicyAdapter(CheckinCheckoutBasePolicyAdapter):
         copier = queryAdapter(self.context, IObjectCopier)
         new_baseline = copier.merge()
 
-        from zc.relation.interfaces import ICatalog
-        from zope.intid.interfaces import IIntIds
-
         catalog = queryUtility(ICatalog)
-        intids = queryUtility(IIntIds)
-        to_rels = catalog.findRelations({'to_id': intids.getId(baseline)})
-        print('to', to_rels)
-        from_rels = catalog.findRelations({'to_id': intids.getId(baseline)})
-        print('from', from_rels)
-
-        for relation in from_rels:
-            if relation is not None:
-                try:
-                    catalog.unindex(relation)
-                except KeyError:
-                    # The relation value has already been unindexed.
-                    pass
-
+        catalog.unindex(relation)
 
         # don't need to unlock the lock disappears with old baseline deletion
         notify(AfterCheckinEvent(new_baseline, checkin_message))
         return new_baseline
+
+    def cancelCheckout(self):
+        relation = self._get_relation_to_baseline()
+        baseline = super(CheckinCheckoutPolicyAdapter, self).cancelCheckout()
+        catalog = queryUtility(ICatalog)
+        catalog.unindex(relation)
+        return baseline
 
     def _get_relation_to_baseline(self):
         # do we have a baseline in our relations?
